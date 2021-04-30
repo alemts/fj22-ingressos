cc09eae6b54d9d73bd37bcf62a554e340167f441
Ale Mattos <alexandrerio.br@gmail.com>
2021-04-29 22:35:26 -0300

    Conclusao do projeto

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -1,6 +1,7 @@
 package br.com.caelum.ingresso.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,55 +18,77 @@ import org.springframework.web.servlet.ModelAndView;
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Carrinho;
+import br.com.caelum.ingresso.model.ImagemCapa;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.TipoDeIngresso;
 import br.com.caelum.ingresso.model.form.SessaoForm;
+import br.com.caelum.ingresso.rest.OmdbClient;
 import br.com.caelum.ingresso.validacao.GerenciadorDeSessao;
 
 @Controller
-public class SessaoController
-{
-  @Autowired
-  private SalaDao salaDao;
+public class SessaoController {
+    @Autowired
+    private SalaDao salaDao;
 
-  @Autowired
-  private FilmeDao filmeDao;
+    @Autowired
+    private FilmeDao filmeDao;
 
-  @Autowired
-  private SessaoDao sessaoDao;
+    @Autowired
+    private SessaoDao sessaoDao;
+    
+    @Autowired
+    private OmdbClient omdbClient;
+    
+    @Autowired
+    private Carrinho carrinho;
+    
 
-  @GetMapping("/admin/sessao")
-  public ModelAndView form(@RequestParam("salaId") Integer salaId, SessaoForm form)
-  {
-    ModelAndView modelAndView = new ModelAndView("sessao/sessao");
+    @GetMapping("/admin/sessao")
+    public ModelAndView form(@RequestParam("salaId") Integer salaId, SessaoForm form) {
+	ModelAndView modelAndView = new ModelAndView("sessao/sessao");
 
-    modelAndView.addObject("sala", salaDao.findOne(salaId));
-    modelAndView.addObject("filmes", filmeDao.findAll());
-    modelAndView.addObject("form", form);
+	modelAndView.addObject("sala", salaDao.findOne(salaId));
+	modelAndView.addObject("filmes", filmeDao.findAll());
+	modelAndView.addObject("form", form);
 
-    return modelAndView;
-  }
-
-  // @PostMapping(value = "/admin/sessao")
-  @PostMapping("/admin/sessao")
-  @Transactional
-  public ModelAndView salva(@Valid SessaoForm form, BindingResult result)
-  {
-    if (result.hasErrors())
-      return form(form.getSalaId(), form);
-
-    Sessao sessao = form.toSessao(salaDao, filmeDao);
-
-    List<Sessao> sessoesDaSala = sessaoDao.buscaSessoesDaSala(sessao.getSala());
-    GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoesDaSala);
-    if (gerenciador.cabe(sessao))
-    {
-      sessaoDao.save(sessao);
-      return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+	return modelAndView;
     }
-    return form(form.getSalaId(), form);
 
-    // sessaoDao.save(sessao);
-    // return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() +
-    // "/sessoes");
-  }
+    // @PostMapping(value = "/admin/sessao")
+    @PostMapping("/admin/sessao")
+    @Transactional
+    public ModelAndView salva(@Valid SessaoForm form, BindingResult result) {
+	if (result.hasErrors())
+	    return form(form.getSalaId(), form);
+
+	Sessao sessao = form.toSessao(salaDao, filmeDao);
+
+	List<Sessao> sessoesDaSala = sessaoDao.buscaSessoesDaSala(sessao.getSala());
+	GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoesDaSala);
+	if (gerenciador.cabe(sessao)) {
+	    sessaoDao.save(sessao);
+	    return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+	}
+	return form(form.getSalaId(), form);
+
+	// sessaoDao.save(sessao);
+	// return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() +
+	// "/sessoes");
+    }
+
+    @GetMapping("/sessao/{id}/lugares")
+    public ModelAndView lugaresNaSessao(@PathVariable("id") Integer sessaoId) {
+	ModelAndView mv = new ModelAndView("sessao/lugares");
+	
+	Sessao sessao = sessaoDao.findOne(sessaoId);
+	Optional<ImagemCapa> imagemCapa = omdbClient.request(sessao.getFilme(), ImagemCapa.class);
+	
+	mv.addObject("sessao", sessao);
+	mv.addObject("imagemCapa", imagemCapa.orElse(new ImagemCapa()));
+	mv.addObject("tiposDeIngressos", TipoDeIngresso.values());
+	mv.addObject("carrinho", carrinho);
+	
+	return mv;
+    }
 }
